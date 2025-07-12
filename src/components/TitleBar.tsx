8870b137a5ddedd1ddbfb5ec9bed3f0f5e39f0ab
Victor Cassiano <43105344+Victorcassiano@users.noreply.github.com>
2025-07-12 00:29:58 -0300

    feat: add OS-specific window controls and update dependencies
    - Added `tauri-plugin-os` for platform detection.
    - Updated `default.json` capabilities to include updater permissions.
    - Refactored `TitleBar` component to use platform-specific controls for macOS, Windows, and Linux.
    - Implemented `LinuxControls`, `MacOsControls`, and `WindowsControls` components for window management.
    - Removed redundant window control logic from `TitleBar`.
    - Created `WindowControlTypes` type definition for better type safety.

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -1,63 +1,30 @@
 import clsx from "clsx";
 import { useTheme } from "../context/ThemeContext";
-import { getCurrentWindow } from "@tauri-apps/api/window";
+
+import { platform } from '@tauri-apps/plugin-os';
+import MacOsControls from "./WindowControl/MacOs";
+import WindowsControls from "./WindowControl/Windows";
+import LinuxControls from "./WindowControl/Linux";
+
+const currentPlatform = platform();
 
 export default function Titlebar() {
   const { theme } = useTheme();
   const appBg = theme === "dark" ? "bg-[#10121b]" : "bg-gray-100";
   const textColor = theme === "dark" ? "text-gray-300" : "text-gray-700";
 
-  const currentWindow = getCurrentWindow();
-
-  const minimizeWindow = async () => {
-    await currentWindow.minimize();
-  };
-
-  const maximizeWindow = async () => {
-    await currentWindow.maximize();
-  };
-
-  const closeWindow = async () => {
-    await currentWindow.close();
-  };
-
   return (
     <div
       className={clsx(
-        "w-full h-8 flex items-center justify-between px-3 cursor-pointer",
+        "w-full h-8 flex items-center justify-between px-3 cursor-default",
         appBg,
         textColor
       )}
       data-tauri-drag-region
     >
-      <div className="flex items-center gap-2">
-        <button
-          onClick={closeWindow}
-          className="w-3 h-3 rounded-full bg-[#ff5f57] hover:bg-[#ff5f57]/90 flex items-center justify-center"
-        >
-          <span className="opacity-0 hover:opacity-100 text-[8px] text-black">
-            ✕
-          </span>
-        </button>
-        <button
-          onClick={minimizeWindow}
-          className="w-3 h-3 rounded-full bg-[#ffbd2e] hover:bg-[#ffbd2e]/90 flex items-center justify-center"
-        >
-          <span className="opacity-0 hover:opacity-100 text-[8px] text-black">
-            -
-          </span>
-        </button>
-        <button
-          onClick={maximizeWindow}
-          className="w-3 h-3 rounded-full bg-[#28c940] hover:bg-[#28c940]/90 flex items-center justify-center"
-        >
-          <span className="opacity-0 hover:opacity-100 text-[8px] text-black">
-            +
-          </span>
-        </button>
-      </div>
-      <div className={clsx("text-sm font-medium", textColor)}>Solo</div>
-      <div className="w-[52px]"></div>
+      {currentPlatform === 'macos' && <MacOsControls appBackground={appBg} textTheme={textColor}  />}
+      {currentPlatform === 'windows' && <WindowsControls appBackground={appBg} textTheme={textColor}  />}
+      {currentPlatform === 'linux' && <LinuxControls appBackground={appBg} textTheme={textColor}  />}
     </div>
   );
 }
